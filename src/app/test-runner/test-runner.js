8c0f2ee489d22736f8893783a47cdb571e18375d
Michel Vollebregt <m.vollebregt@xs4all.nl>
2016-12-22 03:56:53 +0100

    Call jasmine APIs from within angular app

--- src/app/test-runner/test-runner.js
+++ src/app/test-runner/test-runner.js
@@ -1,45 +1,49 @@
-var describes = [];
-var its;
-var expectations = [];
-
-function describe(describeTekst, describeFunc) {
-  describes.push([describeTekst, describeFunc]);
-}
-
-function it(itTekst, itFunc) {
-  its.push([itTekst, function () {
-    var i;
-    expectations = [];
-    itFunc();
-    for (i = 0; i < expectations.length; i++) {
-      expectations[i]();
-    }
-  }]);
-}
-
-function expect(observed) {
-  return {
-    toBe: function(expected) {
-      expectations.push(function() {
-        if (observed !== expected) {
-          throw new Error('Waarde ' + observed + ' is niet gelijk aan verwachte waarde ' + expected);
-        }
-      });
-    }
-  }
-}
-
-window.buildTestSuite = function() {
-  var testSuite = [];
-  var i;
-  for (i = 0; i < describes.length; i++) {
-    its = [];
-    describes[i][1]();
-    testSuite.push([describes[i][0], its]);
-  }
-  return testSuite;
-};
+// var describes = [];
+// var its;
+// var expectations = [];
+//
+// function describe(describeTekst, describeFunc) {
+//   describes.push([describeTekst, describeFunc]);
+// }
+//
+// function it(itTekst, itFunc) {
+//   its.push([itTekst, function () {
+//     var i;
+//     expectations = [];
+//     itFunc();
+//     for (i = 0; i < expectations.length; i++) {
+//       expectations[i]();
+//     }
+//   }]);
+// }
+//
+// function expect(observed) {
+//   return {
+//     toBe: function(expected) {
+//       expectations.push(function() {
+//         if (observed !== expected) {
+//           throw new Error('Waarde ' + observed + ' is niet gelijk aan verwachte waarde ' + expected);
+//         }
+//       });
+//     }
+//   }
+// }
+//
+// window.buildTestSuite = function() {
+//   var testSuite = [];
+//   var i;
+//   for (i = 0; i < describes.length; i++) {
+//     its = [];
+//     describes[i][1]();
+//     testSuite.push([describes[i][0], its]);
+//   }
+//   return testSuite;
+// };
 
 window.onerror = function(message, file, line, col, error) {
   document.write('<h1>Dat gaat niet goed!</h1>' + message);
 };
+
+// window.jasmine = jasmineRequire.core(jasmineRequire);
+// jasmine.getEnv().execute();
+// console.log(jsApiReporter);
--- src/opdracht/opdracht.spec.ts
+++ src/opdracht/opdracht.spec.ts
@@ -1,5 +1,10 @@
 import {MijnKlasse} from "./opdracht";
 
+// TODO: traceur == runtime typescript transpilatie
+// TODO: https://plnkr.co/edit/02G5E4njHo9y7Po00S5g
+
+let env = jasmine.getEnv();
+
 describe ('Opdracht 1', () => {
   it('Definieer een TypeScript klasse MijnKlasse', () => {
     new MijnKlasse();
